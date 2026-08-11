ffd401584f0425793162012856811df321bd0a40
epicever <epicever@gmail.com>
2026-08-10 19:16:36 -0700

    Rename game and add eight-player PeerJS rooms (#9)

--- projects/furry-dockers/source/main.tsx
+++ projects/furry-dockers/source/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import WobbleGame from "./WobbleGame";
+import FurryDockersGame from "./FurryDockersGame";
 import "./standalone.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <WobbleGame />
+    <FurryDockersGame />
   </StrictMode>,
 );
